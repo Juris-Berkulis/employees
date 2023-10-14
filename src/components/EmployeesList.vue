4a85f2940d11a3ad79494afa26c5e9d28d116741
Juris-Berkulis <krztfjrbhm1@gmail.com>
2023-10-14 10:24:13 +0300

    1. Add a button to load a list of employees.

--- src/components/EmployeesList.vue
+++ src/components/EmployeesList.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { computed, type ComputedRef } from 'vue';
+import { computed, ref, type ComputedRef, type Ref, onBeforeUnmount } from 'vue';
 import { storeToRefs } from 'pinia';
 import EmployeesListItem from '@/components/EmployeesListItem.vue';
+import IconSpiner from '@/components/icons/IconSpiner.vue';
 import { type Employee } from '@/data/employees';
 import { useEmployeesDataStore } from '@/stores/employeesData';
 import { useFilterStaffTagStore } from '@/stores/filterStaffTag';
@@ -44,6 +45,19 @@ const {
     isFilterStaffTagEnabled,
 } = storeToRefs(filterStaffTagStore);
 
+const loadStep: number = 4;
+const listPart: Ref<number> = ref(1);
+const isLoading: Ref<boolean> = ref(false);
+const timerId: Ref<ReturnType<typeof setTimeout> | undefined> = ref();
+
+const nextListPart = (): void => {
+    isLoading.value = true;
+    timerId.value = setTimeout(() => {
+        listPart.value++;
+        isLoading.value = false;
+    }, 2000);
+};
+
 const employeesListWithFilteredCountry = useFilterEmployeesBySpecificCriterion(employeesData, filterCountry, 'country');
 const employeesListWithFilteredGender = useFilterEmployeesBySpecificCriterion(employeesListWithFilteredCountry, filterGender, 'gender');
 const employeesListWithFilteredPosition = useFilterEmployeesBySpecificCriterion(employeesListWithFilteredGender, filterPosition, 'position');
@@ -69,6 +83,14 @@ const employeesListWithSorting: ComputedRef<Employee[]> = computed(() => {
         return employee1.status.tag.id - employee2.status.tag.id
     })
 });
+
+const employeesListForShow: ComputedRef<Employee[]> = computed(() => {
+    return employeesListWithSorting.value.slice(0, listPart.value * loadStep)
+});
+
+onBeforeUnmount(() => {
+    clearTimeout(Number(timerId.value));
+});
 </script>
 
 <template>
@@ -77,13 +99,26 @@ const employeesListWithSorting: ComputedRef<Employee[]> = computed(() => {
     <slot class="employees-list__filter-slot" name="filter" />
     <div class="employees-list__items-list">
         <p class="employees-list__empty" v-if="!employeesData.length">Нет сотрудников</p>
-        <p class="employees-list__empty" v-else-if="!employeesListWithSorting.length">По данному запросу сотрудники не найдены</p>
+        <p class="employees-list__empty" 
+            v-else-if="!employeesListForShow.length"
+        >По данному запросу сотрудники не найдены</p>
         <EmployeesListItem 
-            v-for="employee of employeesListWithSorting" 
+            v-for="employee of employeesListForShow" 
             :key="employee.inn" 
             :employee="employee" 
         />
     </div>
+    <button 
+        class="employees-list__more-btn button button_rectangle button_animation"
+        v-if="employeesListForShow < employeesListWithSorting"
+        @click="nextListPart"
+    >
+        <IconSpiner 
+            class="employees-list__more-icon" 
+            :class="{'employees-list__more-icon_stop': !isLoading}"
+        />
+        <span class="employees-list__more-text">Показать еще</span>
+    </button>
 </div>
 </template>
 
@@ -119,6 +154,46 @@ const employeesListWithSorting: ComputedRef<Employee[]> = computed(() => {
     font-size: 20px;
     font-style: normal;
     font-weight: 600;
+    line-height: 120%;
+}
+
+.employees-list__more-btn {
+    display: block;
+    margin: 0 auto 30px;
+    padding: 14px 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    color: var(--blue, #2a358c);
+    border: 1px solid currentColor;
+    box-shadow: $bsh-rectangle rgba(42, 53, 140, 0.20);
+}
+
+.employees-list__more-icon {
+    @keyframes spiner {
+        from {
+            transform: rotateZ(0);
+        }
+
+        to {
+            transform: rotateZ(-360deg);
+        }
+    }
+
+    animation: spiner 1s linear 0s infinite normal;
+
+    &_stop {
+        animation-play-state: paused;
+    }
+}
+
+.employees-list__more-text {
+    color: currentColor;
+    font-family: Montserrat;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
     line-height: 120%;
 }
 </style>
